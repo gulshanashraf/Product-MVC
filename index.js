33aceaca74e16807d3259a2206bfac4643686b50
gul632398-maker <gulshanashraf901@gmail.com>
2026-09-10 20:01:20 +0500

    update

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 connectDB();
 app.use(
   cors({
-    origin: "http://localhost:5173", 
+    origin:"http://localhost:5173", 
     methods: ["GET", "POST" , "PUT", "DELETE"],
   
   })
--- model/product.js
+++ model/product.js
@@ -3,20 +3,35 @@ import mongoose from "mongoose";
 const productSchema = new mongoose.Schema({
   id: {
     type: String,
-    required: true, 
+    required: true,
   },
+
   name: {
     type: String,
   },
+
   price: {
     type: Number,
   },
+
   imageURL: {
     type: String,
   },
+
   desc: {
     type: String,
   },
+
+  category: {
+    type: String,
+  },
+
+  stock: {
+    type: Number,
+    default: 0,
+  },
 });
+
 const Product = mongoose.model("Product", productSchema);
+
 export default Product;
