3407b785ee8a7ab68ac6de38d38ff8fc7ae475b3
gul632398-maker <gulshanashraf901@gmail.com>
2026-09-10 21:10:37 +0500

    update

--- index.js
+++ index.js
@@ -9,9 +9,11 @@ const app = express();
 connectDB();
 app.use(
   cors({
-    origin:"http://localhost:5173", 
-    methods: ["GET", "POST" , "PUT", "DELETE"],
-  
+    origin: [
+      "http://localhost:5173",
+      "https://gulshan-ecommerce.netlify.app",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
 
